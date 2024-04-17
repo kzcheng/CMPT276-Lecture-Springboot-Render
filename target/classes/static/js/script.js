e18f3a4a4a288a150ac6da26c97c8b4515b59cf2
kzcheng <kevinzifancheng@gmail.com>
2024-04-16 17:10:28 -0700

    Format JS file

--- target/classes/static/js/script.js
+++ target/classes/static/js/script.js
@@ -1,64 +1,64 @@
-// var n = window.prompt("name?","Bobby")
-// console.log("hello " + n)
+var n = window.prompt("name?", "Bobby")
+console.log("hello " + n)
 
-// for (var i=0; i<10; i++){
-//     console.log(i)
-// }
+for (var i = 0; i < 10; i++) {
+    console.log(i)
+}
 
-// console.log(i)
+console.log(i)
 
 // objects
 
 var person = {
-    name: 'bobby',
+    name: "bobby",
     age: 42,
-    email: 'bobbyc@sfu.ca',
+    email: "bobbyc@sfu.ca",
     address: {
-        number: '123',
-        street: 'fake st'
+        number: "123",
+        street: "fake st",
     },
-    speak: function(words){
+    speak: function (words) {
         console.log(words)
-    }
+    },
 }
 
 // functions
 
-// var a = 9
+var a = 9
 
-// function myfunc(){
-//     console.log('hello world')
-//     console.log(a)
-// }
+function myfunc() {
+    console.log("hello world")
+    console.log(a)
+}
 
-// var myfunc2 = function(){
-//     console.log('hello world 2')
-// }
+var myfunc2 = function () {
+    console.log("hello world 2")
+}
 
-// myfunc2()
+myfunc2()
 
 // callbacks
 
-// function asc(a,b) {
-//     return a-b
-// }
-// var i = 0
-// var inter = window.setInterval(function(){
-//     console.log(i)
-//     i++
-// },1000)
+function asc(a, b) {
+    return a - b
+}
+var i = 0
+var inter = window.setInterval(function () {
+    console.log(i)
+    i++
+}, 1000)
 
-// person.speak("hello")
+person.speak("hello")
 
-// window.addEventListener('keypress',processKey)
+window.addEventListener("keypress", processKey)
 
-// function processKey(evt){
-//     console.log(evt.key)
-// }
+function processKey(evt) {
+    console.log(evt.key)
+}
 
 var button = document.querySelector('input[value="SuBmIt"]')
-button.addEventListener('click', function(evt){
+button.addEventListener("click", function (evt) {
     evt.preventDefault()
-    var text = document.getElementById('fname').value
-    document.getElementsByTagName('legend')[0].innerHTML = text
+    var text = document.getElementById("fname").value
+    document.getElementsByTagName("legend")[0].innerHTML = text
 })
