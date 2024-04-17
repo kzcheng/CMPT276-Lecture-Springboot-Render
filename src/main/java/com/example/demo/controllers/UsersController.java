bec00a7940c0d39cc425930679033ac550dabf41
kzcheng <kevinzifancheng@gmail.com>
2024-04-16 17:52:26 -0700

    Formatting

--- src/main/java/com/example/demo/controllers/UsersController.java
+++ src/main/java/com/example/demo/controllers/UsersController.java
@@ -2,7 +2,6 @@ package com.example.demo.controllers;
 
 import java.util.List;
 import java.util.Map;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -10,22 +9,20 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.view.RedirectView;
-
 import com.example.demo.models.User;
 import com.example.demo.models.UserRepository;
-
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import jakarta.servlet.http.HttpSession;
 
 @Controller
-public class UsersController {  
-    
+public class UsersController {
+
     @Autowired
     private UserRepository userRepo;
 
     @GetMapping("/users/view")
-    public String getAllUsers(Model model){
+    public String getAllUsers(Model model) {
         System.out.println("Getting all users");
         // get all users from database
         List<User> users = userRepo.findAll();
@@ -35,43 +32,41 @@ public class UsersController {
     }
 
     @GetMapping("/")
-    public RedirectView process(){
+    public RedirectView process() {
         return new RedirectView("login");
     }
 
     @PostMapping("/users/add")
-    public String addUser(@RequestParam Map<String, String> newuser, HttpServletResponse response){
+    public String addUser(@RequestParam Map<String, String> newuser, HttpServletResponse response) {
         System.out.println("ADD user");
         String newName = newuser.get("name");
         String newPwd = newuser.get("password");
         int newSize = Integer.parseInt(newuser.get("size"));
-        userRepo.save(new User(newName,newPwd,newSize));
+        userRepo.save(new User(newName, newPwd, newSize));
         response.setStatus(201);
         return "users/addedUser";
     }
 
     @GetMapping("/login")
-    public String getLogin(Model model, HttpServletRequest request, HttpSession session){
+    public String getLogin(Model model, HttpServletRequest request, HttpSession session) {
         User user = (User) session.getAttribute("session_user");
-        if (user == null){
+        if (user == null) {
             return "users/login";
-        }
-        else {
-            model.addAttribute("user",user);
+        } else {
+            model.addAttribute("user", user);
             return "users/protected";
         }
     }
 
     @PostMapping("/login")
-    public String login(@RequestParam Map<String,String> formData, Model model, HttpServletRequest request, HttpSession session){
+    public String login(@RequestParam Map<String, String> formData, Model model, HttpServletRequest request, HttpSession session) {
         // processing login
         String name = formData.get("name");
         String pwd = formData.get("password");
         List<User> userlist = userRepo.findByNameAndPassword(name, pwd);
-        if (userlist.isEmpty()){
+        if (userlist.isEmpty()) {
             return "users/login";
-        }
-        else {
+        } else {
             // success
             User user = userlist.get(0);
             request.getSession().setAttribute("session_user", user);
@@ -81,7 +76,7 @@ public class UsersController {
     }
 
     @GetMapping("/logout")
-    public String destroySession(HttpServletRequest request){
+    public String destroySession(HttpServletRequest request) {
         request.getSession().invalidate();
         return "/users/login";
     }
